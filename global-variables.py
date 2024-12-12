8746b5c1d0a6bb4e0d35a5259dd16be250d324c1
Mahadev Metre <mahadev.metre666@gmail.com>
2024-12-12 12:58:39 +0530

    added space in z

---
+++ global-variables.py
@@ -0,0 +1,8 @@
+x = "awesome"
+
+def myfunc():
+    z = " man"  # Local variable 'z'
+    y = x + z  # Concatenating 'x' (global) and 'z' (local)
+    print("Python is " + y)  # Proper indentation for the print statement
+
+myfunc()
