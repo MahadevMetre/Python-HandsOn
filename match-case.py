42ef5e47bb9699ee74ac72257106f7f4348bc667
Mahadev Metre <mahadev.metre666@gmail.com>
2024-12-18 22:52:15 +0530

    matchcase

--- match-case.py
+++ match-case.py
@@ -4,5 +4,9 @@ match x:
         print("x is zero")
     case 4:
         print("x is four")
+    case _ if x!= 90:
+        print(x, "is not 90")
+    case _ if x!= 80:
+        print(x, "is not 80")
     case _:
-        print(x)
+        print(x)    
