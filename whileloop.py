2f606b9cdc54a46584dacd5a54acea1c28ca2b01
Mahadev Metre <mahadev.metre666@gmail.com>
2024-12-21 00:10:13 +0530

    while loops

--- whileloop.py
+++ whileloop.py
@@ -1,6 +1,32 @@
-i = 0
-while i < 3:
-    print(i)
-    i += 1
+# i = 0
+# while i < 3:
+#     print(i)
+#     i += 1
 
-print("Done with loop")    
+# print("Done with loop")    
+
+# # scenario2
+# i = 0
+# while i <= 3:
+#     print(i)
+#     i += 1
+
+# print("Done with loop")    
+
+
+# scenario3
+# i = int(input("Enter a number: "))
+
+# while i <= 38:
+#     i = int(input("Enter a number: "))
+#     print(i)
+
+# print("Done with loop")    
+
+# scenario4
+count = 5
+while count > 0:
+    print(count)
+    count = count - 1
+else:
+    print("I am inside else")    
