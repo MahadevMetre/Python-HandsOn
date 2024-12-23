11dc75d05e0ecc81ddb307495d8f2f59fb84a758
Mahadev Metre <mahadev.metre666@gmail.com>
2024-12-23 17:59:10 +0530

    func-arguments

---
+++ func-arguments.py
@@ -0,0 +1,23 @@
+#1
+def average(a=9, b=1):
+    print("The average is ", (a + b) / 2)
+
+    average(b=9)
+
+#2
+def name(fname, mname="Jhon", lname="Whatson"):
+    print("Hello,", fname, mname, lname)
+
+    name("Amy")    
+
+#3
+def average(a, b, c=1):
+    print("The average is ", (a + b + c) / 3)
+
+    average(5, 6)
+
+#4
+def name(fname, mname, lname):
+    print("Hello,", fname, mname, lname)
+
+    name("Peter", "Quill")    
